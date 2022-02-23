1857d6ae21133d69ac5352d3f7c2d473c2d7a05f
abbasio <omarabbasi16@gmail.com>
2022-02-22 22:35:07 -0500

    Added basic typing functionality

--- code/main.js
+++ code/main.js
@@ -2,4 +2,26 @@ import kaboom from 'kaboom';
 
 kaboom();
 
-add([text('Hello World'), pos(120, 80)]);
+let platform = 'test string';
+add([text(platform), pos(480, 320), opacity(0.5)]);
+const untypedWords = platform.split('');
+let typedPlatform = '';
+const typedWords = [];
+
+onCharInput((char) => {
+  typedWords.push(char);
+  const index = typedWords.length - 1;
+  if (typedWords[index] === untypedWords[index]) {
+    typedPlatform += char;
+    add([text(typedPlatform), pos(480, 320), solid(), area()]);
+  } else {
+    typedPlatform += char;
+    add([
+      text(typedPlatform),
+      pos(480, 320),
+      solid(),
+      area(),
+      color(255, 0, 0),
+    ]);
+  }
+});
