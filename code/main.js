ce744a708506c1505ef80b0f936fe9bd3170129a
abbasio <omarabbasi16@gmail.com>
2022-02-22 23:08:40 -0500

    Added letter verification for typing test

--- code/main.js
+++ code/main.js
@@ -3,25 +3,34 @@ import kaboom from 'kaboom';
 kaboom();
 
 let platform = 'test string';
-add([text(platform), pos(480, 320), opacity(0.5)]);
 const untypedWords = platform.split('');
-let typedPlatform = '';
+
+untypedWords.forEach((letter, index) => {
+  add([text(letter), pos(index * 50 + 480, 320), opacity(0.5)]);
+});
+
 const typedWords = [];
+
+const whiteLetter = (char, index) => {
+  add([text(char), pos(index * 50 + 480, 320), solid(), area()]);
+};
+
+const redLetter = (char, index) => {
+  add([
+    text(char),
+    pos(index * 50 + 480, 320),
+    solid(),
+    area(),
+    color(255, 0, 0),
+  ]);
+};
 
 onCharInput((char) => {
   typedWords.push(char);
   const index = typedWords.length - 1;
   if (typedWords[index] === untypedWords[index]) {
-    typedPlatform += char;
-    add([text(typedPlatform), pos(480, 320), solid(), area()]);
+    whiteLetter(char, index);
   } else {
-    typedPlatform += char;
-    add([
-      text(typedPlatform),
-      pos(480, 320),
-      solid(),
-      area(),
-      color(255, 0, 0),
-    ]);
+    redLetter(char, index);
   }
 });
