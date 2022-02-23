59265d66bddfd83e35aa1f70451f77bb80381f2f
abbasio <omarabbasi16@gmail.com>
2022-02-23 12:06:39 -0500

    Added runner

--- code/main.js
+++ code/main.js
@@ -2,23 +2,44 @@ import kaboom from 'kaboom';
 
 kaboom();
 
+loadSprite('runner', '../public/sprites/run.png', {
+  sliceX: 8,
+  anims: {
+    run: {
+      from: 0,
+      to: 7,
+      loop: true,
+    },
+  },
+});
+
+add([
+  sprite('runner', {
+    anim: 'run',
+  }),
+  area(),
+  pos(480, 120),
+  scale(2),
+  body(),
+  move(RIGHT, 50),
+]);
 let platform = 'test string';
 const untypedWords = platform.split('');
 
 untypedWords.forEach((letter, index) => {
-  add([text(letter), pos(index * 50 + 480, 320), opacity(0.5)]);
+  add([text(letter), pos(index * 50 + 480, 620), opacity(0.5)]);
 });
 
 const typedWords = [];
 
 const whiteLetter = (char, index) => {
-  add([text(char), pos(index * 50 + 480, 320), solid(), area()]);
+  add([text(char), pos(index * 50 + 480, 620), solid(), area()]);
 };
 
 const redLetter = (char, index) => {
   add([
     text(char),
-    pos(index * 50 + 480, 320),
+    pos(index * 50 + 480, 620),
     solid(),
     area(),
     color(255, 0, 0),
